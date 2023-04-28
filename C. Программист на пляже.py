da3f9ee1d438596f387b1faead702f5e15a681e1
Igor Jarvinen <stameskaa@inbox.ru>
2023-04-28 22:09:00 +0300

    [upd] Откорректированы отступы

--- C. Программист на пляже.py
+++ C. Программист на пляже.py
@@ -1,13 +1,13 @@
 t = int(input()) # кол-во тестов
 
 for test in range(t):
-	n = int(input()) # кол-во лежаков
-	a = [int(num) for num in input().split()] # идентификаторы лежаков
-	similar = 10 ** 9 # непохожесть
+    n = int(input()) # кол-во лежаков
+    a = [int(num) for num in input().split()] # идентификаторы лежаков
+    similar = 10 ** 9 # непохожесть
 
-	for i in a:
-		for j in a:
-			if i != j and i ^ j < similar: # i ^ j - XOR
-				similar = i ^ j
+    for i in a:
+        for j in a:
+            if i != j and i ^ j < similar: # i ^ j - XOR
+                similar = i ^ j
 
-	print(similar)
+    print(similar)
